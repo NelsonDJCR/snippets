6a828386583fa9c51965cc852006a944af39e60e
NelsonDJCR <njjimenez21@misena.edu.co>
2021-05-27 10:10:12 -0500

    NelsonDJCR jue. 27/05/2021 - 10:10:12,82

---
+++ pull.bat
@@ -0,0 +1,4 @@
+ECHO Descargando cambios...
+git pull
+ECHO Perfectos, todos los cambios han sido realizadosAS
+PAUSE
---
+++ push.bat
@@ -0,0 +1,6 @@
+ECHO Subiendo cambios
+git add .
+git commit -m "NelsonDJCR %date% - %time%"
+git push origin main
+ECHO Perfectos, todos los cambios han sido realizados
+PAUSE
