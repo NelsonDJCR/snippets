a4ffe5da884dea5073a389265bf2599a8ada5b11
NelsonDJCR <njjimenez21@misena.edu.co>
2021-05-29 08:26:00 -0500

    NelsonDJCR sáb. 29/05/2021 -  8:26:00,07

--- pull.bat
+++ pull.bat
@@ -1,4 +1,11 @@
+color 0b
 ECHO Descargando cambios...
+ECHO "o   o             o                              o-o           o       o-o     o--o " 
+ECHO "|\  |             |                              |  \          |      /        |   |" 
+ECHO "| \ |     o-o     |     o-o     o-o     o-o      |   O         |     O         O-Oo " 
+ECHO "|  \|     |-'     |      \      | |     |  |     |  /      \   o      \        |  \ " 
+ECHO "o   o     o-o     o     o-o     o-o     o  o     o-o        o-o        o-o     o   o" 
+ECHO
 git pull
 ECHO Perfectos, todos los cambios han sido realizadosAS
 PAUSE
